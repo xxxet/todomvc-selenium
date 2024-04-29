a643a87152371ab652ca364741aa88c20323164c
dmitrykarpenko <dmitrii.kar@gmail.com>
2024-04-29 15:46:04 +0300

    updated container versions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+python3 -m venv venv
+source ./venv/bin/activate
 pip install -r requirements.txt -U
 pytest --browser=chrome -n 2 --alluredir=$1 --remote_driver='http://selenium-hub:4444/wd/hub'
